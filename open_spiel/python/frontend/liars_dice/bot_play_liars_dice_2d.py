8ad6a0154ab08481710c082c93c68d5327c7d6b9
Nathan Groover <nathan.groover@gmail.com>
2023-12-21 21:52:41 -0500

    Allow selecting different models to play against random bot

--- open_spiel/python/frontend/liars_dice/bot_play_liars_dice_2d.py
+++ open_spiel/python/frontend/liars_dice/bot_play_liars_dice_2d.py
@@ -12,100 +12,107 @@ from open_spiel.python.algorithms import deep_cfr_tf2
 from open_spiel.python.algorithms import exploitability
 from open_spiel.python import policy
 from open_spiel.python.bots.uniform_random import UniformRandomBot
+import database
 
 class PolicyNetworkBot(pyspiel.Bot):
-  """Samples an action from action probabilities based on a policy network.
-  """
-
-  def __init__(self, player_id, rng, policy):
-    """Initializes a policy bot.
-
-    Args:
-      player_id: The integer id of the player for this bot, e.g. `0` if acting
-        as the first player.
-      rng: A random number generator supporting a `choice` method, e.g.
-        `np.random`
-      policy: A policy to get action distributions
+    """Samples an action from action probabilities based on a policy network.
     """
-    pyspiel.Bot.__init__(self)
-    self._player_id = player_id
-    self._rng = rng
-    self._policy = policy
 
-  def player_id(self):
-    return self._player_id
+    def __init__(self, player_id, rng, policy):
+        """Initializes a policy bot.
 
-  def restart_at(self, state):
-    pass
+        Args:
+          player_id: The integer id of the player for this bot, e.g. `0` if acting
+            as the first player.
+          rng: A random number generator supporting a `choice` method, e.g.
+            `np.random`
+          policy: A policy to get action distributions
+        """
+        pyspiel.Bot.__init__(self)
+        self._player_id = player_id
+        self._rng = rng
+        self._policy = policy
 
-  def step_with_policy(self, state):
-    cur_player = state.current_player()
-    legal_actions = state.legal_actions(cur_player)
-    legal_action_mask = tf.constant(state.legal_actions_mask(cur_player), dtype=tf.float32)
-    info_state_vector = tf.constant(state.information_state_tensor(), dtype=tf.float32)
-    if len(info_state_vector.shape) == 1:
-      info_state_vector = tf.expand_dims(info_state_vector,axis=0)
-    if len(legal_action_mask.shape) == 1:
-      legal_action_mask = tf.expand_dims(legal_action_mask,axis=0)
+    def player_id(self):
+        return self._player_id
 
-    inp = (info_state_vector, legal_action_mask)
-    probs = self._policy.call(inp).numpy()
-    action_probs = [probs[0][action] for action in legal_actions]
-    #print(legal_actions)
-    #print(action_probs)
-    action_probs=np.array(action_probs)
-    #print(action_probs)
-    #print(info_state_vector)
-    #print(legal_action_mask)
-    chosen_action = self._rng.choice(legal_actions, p=action_probs)
-    #for i,x in enumerate(legal_actions):
-        #print(f'{x} : {state.action_to_string(x)} : {action_probs[i]}')
-    #print(f'choosing {chosen_action}')
-    #print('----')
-    return chosen_action
+    def restart_at(self, state):
+        pass
 
-  def step(self, state):
-    return self.step_with_policy(state)
+    def step_with_policy(self, state):
+        cur_player = state.current_player()
+        legal_actions = state.legal_actions(cur_player)
+        legal_action_mask = tf.constant(state.legal_actions_mask(cur_player), dtype=tf.float32)
+        info_state_vector = tf.constant(state.information_state_tensor(), dtype=tf.float32)
+        if len(info_state_vector.shape) == 1:
+            info_state_vector = tf.expand_dims(info_state_vector,axis=0)
+        if len(legal_action_mask.shape) == 1:
+            legal_action_mask = tf.expand_dims(legal_action_mask,axis=0)
+
+        inp = (info_state_vector, legal_action_mask)
+        probs = self._policy.call(inp).numpy()
+        action_probs = [probs[0][action] for action in legal_actions]
+        #print(legal_actions)
+        #print(action_probs)
+        action_probs=np.array(action_probs)
+        #print(action_probs)
+        #print(info_state_vector)
+        #print(legal_action_mask)
+        chosen_action = self._rng.choice(legal_actions, p=action_probs)
+        #for i,x in enumerate(legal_actions):
+            #print(f'{x} : {state.action_to_string(x)} : {action_probs[i]}')
+        #print(f'choosing {chosen_action}')
+        #print('----')
+        return chosen_action
+
+    def step(self, state):
+        return self.step_with_policy(state)
 
 def main():
-  policy_network = tf.keras.models.load_model('policy_network',compile=False)
-  NUM_GAMES=10000
-  #NUM_GAMES=15
-  p1_wins=0
-  p2_wins=0
-  draws=0
-  bots=[PolicyNetworkBot(0,np.random,policy_network),UniformRandomBot(1,np.random)]
-  game = pyspiel.load_game('liars_dice(numdice=2)')
+    game_name = input("game_name: ")
+    print('Agents:')
+    database.listAgentModels(game_name)
+    agent_name = input("name: ")
+    model_folder='player1_model'
+    database.dumpAgentModel(agent_name, model_folder)
+    policy_network = tf.keras.models.load_model(model_folder,compile=False)
+    NUM_GAMES=10000
+    #NUM_GAMES=15
+    p1_wins=0
+    p2_wins=0
+    draws=0
+    bots=[PolicyNetworkBot(0,np.random,policy_network),UniformRandomBot(1,np.random)]
+    game = pyspiel.load_game(game_name)
 
-  for _ in range(NUM_GAMES):
-    state = game.new_initial_state()
-    for bot in bots:
-      bot.restart_at(state)
-    while not state.is_terminal():
-      if state.is_chance_node():
-        outcomes, probs = zip(*state.chance_outcomes())
-        action = np.random.choice(outcomes, p=probs)
-        state.apply_action(action)
-      else:
-        #print(f'state {state}')
-        current_player = state.current_player()
-        action = bots[current_player].step(state)
-        state.apply_action(action)
-    if state.returns()[1] > 0:
-      p2_wins+=1
-      #print(f'state {state}')
-      #print(f'p2 (random) wins!')
-    elif state.returns()[0] > 0:
-      p1_wins+=1
-      #print(f'p1 (policy) wins!')
-      #print(f'state {state}')
-    else:
-      draws+=1
-  print(f'p1 (policy) wins: {p1_wins} {p1_wins/NUM_GAMES*100} %')
-  print(f'p2 (random) wins: {p2_wins} {p2_wins/NUM_GAMES*100} %')
-  print(f'draws: {draws}')
+    for _ in range(NUM_GAMES):
+        state = game.new_initial_state()
+        for bot in bots:
+            bot.restart_at(state)
+        while not state.is_terminal():
+            if state.is_chance_node():
+                outcomes, probs = zip(*state.chance_outcomes())
+                action = np.random.choice(outcomes, p=probs)
+                state.apply_action(action)
+            else:
+                #print(f'state {state}')
+                current_player = state.current_player()
+                action = bots[current_player].step(state)
+                state.apply_action(action)
+        if state.returns()[1] > 0:
+            p2_wins+=1
+            #print(f'state {state}')
+            #print(f'p2 (random) wins!')
+        elif state.returns()[0] > 0:
+            p1_wins+=1
+            #print(f'p1 (policy) wins!')
+            #print(f'state {state}')
+        else:
+            draws+=1
+    print(f'p1 (policy) wins: {p1_wins} {p1_wins/NUM_GAMES*100} %')
+    print(f'p2 (random) wins: {p2_wins} {p2_wins/NUM_GAMES*100} %')
+    print(f'draws: {draws}')
 
 
 if __name__ == '__main__':
-  main()
+    main()
 
--- open_spiel/python/frontend/liars_dice/database.py
+++ open_spiel/python/frontend/liars_dice/database.py
@@ -8,6 +8,10 @@ agent_table_create_query = "CREATE TABLE IF NOT EXISTS agents(name string PRIMAR
 
 insert_agent_query = "INSERT INTO agents (name, game_name, start_time, finish_time, model, policy_layers, advantage_layers, num_iterations, traversals, learning_rate, batch_size_advantage, batch_size_strategy, memory_capacity) VALUES(?, ?, datetime(?, 'unixepoch'), datetime('now'), ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
+select_agents_by_game_query = "SELECT name FROM agents WHERE game_name = ?;"
+
+select_agent_by_name= "SELECT model FROM agents WHERE name = ?;"
+
 def saveModelToDB(cfrsolver, model_folder, start_time):
     zipname=model_folder+'.zip'
     if os.path.isfile(zipname):
@@ -37,9 +41,35 @@ def saveModelToDB(cfrsolver, model_folder, start_time):
 
     cur.close()
 
+def writeBytesToFile(filename, data):
+    with open(filename, 'wb') as file:
+        file.write(data)
+
 def convertToBinaryData(filename):
     # Convert digital data to binary format
     with open(filename, 'rb') as file:
         blobData = file.read()
     return blobData
 
+def listAgentModels(game):
+    conn = sqlite3.connect('games.db')
+    cur = conn.cursor()
+    results = cur.execute(select_agents_by_game_query, (game,))
+    for row in results:
+        print("" +row[0])
+    cur.close()
+
+def dumpAgentModel(name, folder):
+    temp_zip = "temp.zip"
+    conn = sqlite3.connect('games.db')
+    cur = conn.cursor()
+    results = cur.execute(select_agent_by_name, (name,))
+    first_result=results.fetchone()
+    if os.path.isdir(folder):
+        shutil.rmtree(folder)
+    os.mkdir(folder)
+    full_temp_path=os.path.join(folder, temp_zip)
+    writeBytesToFile(full_temp_path, first_result[0])
+    shutil.unpack_archive(full_temp_path, folder, 'zip',)
+    
+    
